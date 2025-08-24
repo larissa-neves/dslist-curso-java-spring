31a01ba23dc7a234db10ea5893d2f0b5f74382c5
Larissa Neves <larissaneves6089@gmail.com>
2025-08-24 20:19:39 -0300

    feat: add GET game by id

--- src/main/java/com/cursodevsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/cursodevsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.cursodevsuperior.dslist.controllers;
 
+import com.cursodevsuperior.dslist.dto.GameDTO;
 import com.cursodevsuperior.dslist.dto.GameMinDTO;
 import com.cursodevsuperior.dslist.entities.Game;
 import com.cursodevsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +21,15 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}") //mapeando método http
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
     @GetMapping //mapeando método http
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
-
 }
--- src/main/java/com/cursodevsuperior/dslist/services/GameService.java
+++ src/main/java/com/cursodevsuperior/dslist/services/GameService.java
@@ -1,9 +1,11 @@
 package com.cursodevsuperior.dslist.services;
 
 
+import com.cursodevsuperior.dslist.dto.GameDTO;
 import com.cursodevsuperior.dslist.dto.GameMinDTO;
 import com.cursodevsuperior.dslist.entities.Game;
 import com.cursodevsuperior.dslist.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    //Busca o game no BD e converte para DTO
+    @Transactional(readOnly = true) //garantir que as op com o BD aconteça obedencendo os principios das transações
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
